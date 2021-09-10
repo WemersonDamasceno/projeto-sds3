92e4fc8cdc15959c2e435a9d13fdaf103af78a32
WemersonDamasceno <wemersondamasceno7@gmail.com>
2021-09-10 12:34:32 -0300

    Layers - Criacao das camadas do backend

--- backend/src/main/java/com/devsuperior/dsvendas/DsvendasApplication.java
+++ backend/src/main/java/com/devsuperior/dsvendas/DsvendasApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DsvendasApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DsvendasApplication.class, args);
 	}
