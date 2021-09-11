545c9b491bdf09916c84f5b13d7d7f1a36884702
WemersonDamasceno <wemersondamasceno7@gmail.com>
2021-09-11 12:54:52 -0300

    First request - Usando o axios para buscar os dados de DonutChart

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,10 +1,29 @@
+import axios from 'axios';
 import Chart from 'react-apexcharts';
+import { SaleSum } from 'types/sale';
+import { BASE_URL } from 'utils/requests';
 
+type ChartData = {
+    labels: string[];
+    series: number[];
+}
 function DonutChart() {
-    const mockData = {
+    //ERRADA
+    let chartData : ChartData = {labels:[], series:[]};
+
+    axios.get(BASE_URL + "sales/amount-by-seller")
+    .then(res => {
+        const data = res.data as SaleSum[];
+        const myLabel = data.map(x => x.sellerName);
+        const mySeries = data.map(x => x.sum);
+
+        chartData = {labels: myLabel, series: mySeries}
+    });
+
+    /*const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
+    }*/
 
     const options = {
         legend: {
@@ -13,8 +32,8 @@ function DonutChart() {
     }
     return (
         <Chart
-            options={{ ...options, labels: mockData.labels }}
-            series={mockData.series}
+            options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
             type="donut"
             height="240" />
     );
