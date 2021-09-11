27feb3113a2e58bdff6fc1d54d4a0e2bec55ba55
WemersonDamasceno <wemersondamasceno7@gmail.com>
2021-09-11 15:42:36 -0300

    Adiconando mudancas na navegacao com Primeiro e Ultimo

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,31 +1,38 @@
+import { BiArrowToLeft, BiArrowToRight } from "react-icons/bi";
 import { SalePage } from "types/sale";
 
 type Props = {
     page: SalePage,
     onPageChange: Function
 }
-
-function Pagination({page, onPageChange}: Props) {
+ 
+function Pagination({ page, onPageChange }: Props) {
     return (
         <div className="row d-flex justify-content-center">
-        <nav>
-            <ul className="pagination">
-                <li className={`page-item ${page.first ? 'disable' : ''} `}>
-                    <button className="page-link" onClick={()=>
-                        onPageChange(page.number - 1)
-                    }>Anterior</button>
-                </li>
-                <li className="page-item disabled">
-                    <span className="page-link">{page.number + 1}</span>
-                </li>
-                <li className={`page-item ${page.last ? 'disable' : ''}`}>
-                <button className="page-link" onClick={()=>
-                        onPageChange(page.number + 1)
-                    }>Próxima</button>
-                </li>
-            </ul>
-        </nav>
-    </div>
+            <nav>
+                <ul className="pagination">
+                    <li className={`page-item ${page.number === 0 ? 'disabled' : ''}`}>
+                        <button className="page-link" onClick={() => onPageChange(0)}><BiArrowToLeft /> Primeira</button>
+                    </li>
+                    <li className={`page-item ${page.first ? 'disabled' : ''}`}>
+                        <button className="page-link" onClick={() =>
+                            onPageChange(page.number - 1)
+                        }>Anterior</button>
+                    </li>
+                    <li className="page-item disabled">
+                        <span className="page-link">{page.number + 1}</span>
+                    </li>
+                    <li className={`page-item ${page.last ? 'disabled' : ''}`}>
+                        <button className="page-link" onClick={() =>
+                            onPageChange(page.number + 1)
+                        }>Próxima</button>
+                    </li>
+                    <li className={`page-item ${page.number === page.totalPages - 1 ? 'disabled' : ''}`}>
+                        <button className="page-link" onClick={() => onPageChange(page.totalPages - 1)}>Última <BiArrowToRight /></button>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     )
 }
 export default Pagination;
