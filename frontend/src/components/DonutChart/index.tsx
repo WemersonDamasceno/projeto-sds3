ec35b7a131c0eb7b1d934072a5ad8ad76c0ca5ca
WemersonDamasceno <wemersondamasceno7@gmail.com>
2021-09-11 14:53:11 -0300

    DataTable integration - Fazendo requisição da tabela, e adicionando uma formato da moeda

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -22,13 +22,6 @@ function DonutChart() {
             });
     }, []);
 
-
-
-    /*const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }*/
-
     const options = {
         legend: {
             show: true
@@ -36,6 +29,7 @@ function DonutChart() {
     }
     return (
         <Chart
+            
             options={{ ...options, labels: chartData.labels }}
             series={chartData.series}
             type="donut"
--- frontend/src/utils/format.ts
+++ frontend/src/utils/format.ts
@@ -1,4 +1,18 @@
+import { format } from "date-fns";
+
 export const round = (value: number, precision: number) => {
     var multiplier = Math.pow(10, precision || 0);
     return Math.round(value * multiplier) / multiplier;
+}
+
+export const formatLocalDate = (date: string, pattern: string) => {
+    const dt = new Date(date);
+    const dtDateOnly = new Date(dt.valueOf() + dt.getTimezoneOffset() * 60 * 1000);
+    return format(dtDateOnly, pattern);
+}
+
+export const formatMoeda = (numero1: number) => {
+    var numero = numero1.toFixed(2).split('.');
+    numero[0] = "R$ " + numero[0].split(/(?=(?:...)*$)/).join('.');
+    return numero.join(',');
 }
